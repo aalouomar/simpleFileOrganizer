bed9c4ea6fec4ea9cc9ef3fce3f7ff82b7fe3973
aalouomar <omaraalou.@gmail.com>
2022-05-26 16:26:25 +0100

    Updated main.py to avoid manipulating directories

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import os
 import shutil
 
 # The Path of the directory to be sorted
-path = 'C:\\Users\\<USERNAME>\\Downloads'
+path = 'C:\\Users\\OMAR\\Downloads'
 # This populates a list with the filenames in the directory
 fileList = os.listdir(path)
 
 for file_ in fileList:
     name, ext = os.path.splitext(file_)
-    print(name)
     ext = ext[1:]
-    if ext == '':
+    if os.path.isdir(f'{path}/{file_}'):
         continue
     if not os.path.exists(f'{path}/{ext}'):
         os.makedirs(f'{path}/{ext}')
     shutil.move(f'{path}/{file_}', f'{path}/{ext}/{file_}')
+    print(f'{file_} is moved')
