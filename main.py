b3a2b01346935791164c18c6643f38b34bc641dc
aalouomar <omaraalou.@gmail.com>
2022-05-30 15:17:40 +0100

    Change the sorted files organization

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from pathlib import Path
 
 targetDir = Path(r'D:\Download')
-directories = {"HTML": [".html5", ".html", ".htm", ".xhtml"],
-               "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg", ".heif", ".psd"],
-               "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng", ".qt", ".mpg", ".mpeg",
-                          ".3gp"],
-               "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods", ".odt", ".pwi", ".xsn",
-                             ".xps", ".dotx", ".docm", ".dox", ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx",
-                             ".ppt", "pptx"],
-               "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
-                            ".dmg", ".rar", ".xar", ".zip"],
-               "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
-                         ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
-               "PLAINTEXT": [".txt", ".in", ".out"],
-               "PDF": [".pdf"],
-               "PYTHON": [".py"],
-               "XML": [".xml"],
-               "EXE": [".exe", ".apk"],
-               "SHELL": [".sh"]
+directories = {"IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg", ".heif", ".psd"],
+               "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng", ".qt", ".mpg",
+                          ".mpeg", ".3gp"],
+               "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3", ".msv", "ogg",
+                         "oga", ".raw", ".vox", ".wav", ".wma"],
+               "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",".dmg", ".rar",
+                            ".xar", ".zip"],
+               "APPS": [".exe", ".apk"],
+               "DOCUMENTS": [".oxps", ".pages", ".fdf", ".ods", ".odt", ".pwi", ".xsn",
+                             ".xps", ".dotx", ".docm", ".dox", ".rvg", ".rtf", ".rtfd", ".wpd"],
+               "DOCUMENTS\\PLAINTEXT": [".txt", ".in", ".out"],
+               "DOCUMENTS\\PDF": [".pdf"],
+               "DOCUMENTS\\WORD": [".docx", ".doc"],
+               "DOCUMENTS\\EXCEL": [".xls", ".xlsx"],
+               "DOCUMENTS\\POWERPOINT": [".ppt", "pptx"],
+               "DOCUMENTS\\EBOOKS": [".epub", ".mobi"],
+               "DEV\\PYTHON": [".py"],
+               "DEV\\XML": [".xml"],
+               "DEV\\SHELL": [".sh"],
+               "DEV\\HTML": [".html5", ".html", ".htm", ".xhtml"]
                }
 
 fileList = [x for x in targetDir.rglob('*')
